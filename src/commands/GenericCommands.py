34c4d72482ba7c027337952c472bb202cc6cd345
Austin <56428356+Riigess@users.noreply.github.com>
2025-05-26 09:17:41 -0700

    Cleaned up database-test.sqlite, removed Amazon command from wrong file

--- src/commands/GenericCommands.py
+++ src/commands/GenericCommands.py
@@ -11,22 +11,3 @@ class GenericCommands(commands.Cog):
     @commands.has_role("Mod")
     async def config(self, ctx, command_name:str, change_to:str):
        pass
-
-    @commands.command(pass_context=True)
-    async def amazon(self, ctx, url:str):
-        ref_endian = self.dbh.get_command_alias_response(ctx.guild.id, 'amazon')
-        if len(ref_endian) == 0:
-            return
-        if ref_endian[0] == '/':
-            ref_endian = ref_endian[1:]
-        has_embed = '<' not in url and '>' not in url
-        if not has_embed:
-            url = url[1:-1]
-        if '<' == url[0]:
-            url = url[1:]
-        if '>' == url[-1]:
-            url = url[:-1]
-        if "amazon.com" in url:
-            await ctx.message.reply(f"Not required, but this would be helpful to support the community!\n{url}/{ref_endian}")
-        if has_embed:
-            await ctx.message.edit(suppress=True)
